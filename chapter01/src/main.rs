8738ad51c0de0f2f453c0fddd87e646b1a1f8111
Aoi1011 <ht.so.ik0102@gmail.com>
2023-01-07 21:44:20 +0700

    Implement numbers

--- chapter01/src/main.rs
+++ chapter01/src/main.rs
@@ -2,11 +2,20 @@ use std::sync::Arc;
 use std::{rc::Rc, thread};
 
 fn main() {
-    let a = Arc::new([1, 2, 3]);
-    let b = a.clone();
+    let numbers = vec![1, 2, 3];
 
-    assert_eq!(a.as_ptr(), b.as_ptr());
-
-    thread::spawn(move || dbg!(a));
-    thread::spawn(move || dbg!(b));
+    thread::spawn(move || {
+        for n in numbers {
+            println!("{n}");
+        }
+    })
+    .join()
+    .unwrap();
 }
+
+// fn f() {
+//     println!("Hello from another thread!");
+
+//     let id = thread::current().id();
+//     println!("This is my thread id: {id:?}");
+// }
