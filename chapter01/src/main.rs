70963b707208f5bfca0f1331fb4964666c56b3ac
Aoi1011 <ht.so.ik0102@gmail.com>
2023-01-08 14:38:09 +0700

    Update

--- chapter01/src/main.rs
+++ chapter01/src/main.rs
@@ -1,17 +1,55 @@
+use core::num;
+use std::ops::Add;
 use std::sync::Arc;
 use std::{rc::Rc, thread};
 
 fn main() {
-    let numbers: Vec<usize> = vec![1, 2, 3];
+    // let a = Arc::new([1, 2, 3]);
+    // let b = a.clone();
 
-    let t = thread::spawn(|| {
-        println!("Thread name: {}", thread::current().name().unwrap());
-        let len = numbers.len();
-        let sum = numbers.into_iter().sum::<usize>();
-        sum / len
-    });
+    // thread::spawn(move || {
+    //     dbg!(a);
+    // });
+    // thread::spawn(move || dbg!(b));
+    ownership();
+}
 
-    let average = t.join().unwrap();
+fn f(a: &i32, b: &mut i32, c: i32) {
+    // let hello = String::from("Hello");
+    // let hello2 = hello;
+    // let before = *a;
+    // *b += 1;
+    // let after = a;
+    // let some = c;
+    // let any = c;
 
-    println!("average: {average}");
+    // println!("{} {}", hello, c);
+    // if before != after {
+    //     x
+    // }
+}
+
+fn ownership() {
+    let mut s = String::from("hello");
+    s.push_str(", world");
+
+    println!("{s}");
+
+    let numbers = vec![1, 2, 3];
+    
+    for num in numbers {
+        println!("{}", num);
+    }
+
+    let index: usize = 0;
+
+    match index {
+        0 => println!("0"),
+        1 => println!("1"),
+        _ => println!("other")
+    }
+
+    let a = [123, 345, 789];
+    let b = unsafe {a.get_unchecked(index)};
+    println!("{}", b);
 }
