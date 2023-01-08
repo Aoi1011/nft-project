4b93929d342475dac8fc6271c7bf04c8522497da
Aoi1011 <ht.so.ik0102@gmail.com>
2023-01-08 11:28:38 +0700

    Print thread name

--- chapter01/src/main.rs
+++ chapter01/src/main.rs
@@ -2,9 +2,10 @@ use std::sync::Arc;
 use std::{rc::Rc, thread};
 
 fn main() {
-    let numbers: Vec<usize> = vec![];
+    let numbers: Vec<usize> = vec![1, 2, 3];
 
     let t = thread::spawn(|| {
+        println!("Thread name: {}", thread::current().name().unwrap());
         let len = numbers.len();
         let sum = numbers.into_iter().sum::<usize>();
         sum / len
