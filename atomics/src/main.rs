e563c77c940a2f4165d861078b9fb831f065a485
Aoi1011 <ht.so.ik0102@gmail.com>
2023-01-09 22:37:16 +0700

    Create atomics

---
+++ atomics/src/main.rs
@@ -0,0 +1,64 @@
+use std::{
+    cell::UnsafeCell,
+    sync::atomic::{AtomicBool, Ordering},
+    thread,
+};
+
+const LOCKED: bool = true;
+const UNLOCKED: bool = false;
+
+struct Mutex<T> {
+    locked: AtomicBool,
+    v: UnsafeCell<T>,
+}
+
+unsafe impl<T> Sync for Mutex<T> where T: Send {}
+
+impl<T> Mutex<T> {
+    pub fn new(t: T) -> Self {
+        Self {
+            locked: AtomicBool::new(UNLOCKED),
+            v: UnsafeCell::new(t),
+        }
+    }
+
+    pub fn with_lock<R>(&self, f: impl FnOnce(&mut T) -> R) -> R {
+        while self
+            .locked
+            .compare_exchange_weak(UNLOCKED, LOCKED, Ordering::Relaxed, Ordering::Relaxed)
+            .is_err()
+        {
+            // MESI protocol: stay in S when locked
+            while self.locked.load(Ordering::Relaxed) == LOCKED {
+                thread::yield_now();
+            }
+            thread::yield_now();
+        }
+        // Safety: we hold the lock, therefore we can create mutable reference.
+        let ret = f(unsafe { &mut *self.v.get() });
+        self.locked.store(UNLOCKED, Ordering::Relaxed);
+        ret
+    }
+}
+
+fn main() {
+    let l: &'static _ = Box::leak(Box::new(Mutex::new(0)));
+
+    let handles: Vec<_> = (0..100)
+        .map(|_| {
+            thread::spawn(move || {
+                for _ in 0..1000 {
+                    l.with_lock(|v| {
+                        *v += 1;
+                    });
+                }
+            })
+        })
+        .collect();
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    assert_eq!(l.with_lock(|v| *v), 100 * 1000);
+}
